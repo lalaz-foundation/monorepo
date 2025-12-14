c1f96092a7acf93c37ac4703b26a1c8a7f42bae0
Gregory Serrao <gregperes84@gmail.com>
2025-12-14 18:39:22 -0500

    fix: use HttpApplication::boot instead of create in app templates

--- packages/app/templates/web/public/index.php
+++ packages/app/templates/web/public/index.php
@@ -4,7 +4,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Lalaz\Runtime\Http\HttpApplication;
 
-$app = HttpApplication::create(__DIR__.'/..');
+$app = HttpApplication::boot(__DIR__.'/..');
 
 $response = $app->handleRequest();
 $app->sendResponse($response);
