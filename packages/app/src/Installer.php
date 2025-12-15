843dee13cd49961019d88dc8de9181d610481832
Gregory Serrao <gregperes84@gmail.com>
2025-12-15 11:43:11 -0500

    fix: bump installer version to rc.3

--- packages/app/src/Installer.php
+++ packages/app/src/Installer.php
@@ -6,7 +6,7 @@ namespace Lalaz\Installer;
 
 class Installer
 {
-    private const VERSION = '1.0.0-rc.2';
+    private const VERSION = '1.0.0-rc.3';
 
     private static array $colors = [
         'reset' => "\033[0m",
@@ -184,7 +184,7 @@ class Installer
             'license' => 'MIT',
             'require' => [
                 'php' => '^8.3',
-                'lalaz/framework' => '^1.0.0-rc.2',
+                'lalaz/framework' => '^1.0.0-rc.3',
             ],
             'autoload' => [
                 'psr-4' => [
@@ -197,25 +197,25 @@ class Installer
 
         // Add packages based on features
         if ($projectType === 'web') {
-            $composer['require']['lalaz/web'] = '^1.0.0-rc.2';
+            $composer['require']['lalaz/web'] = '^1.0.0-rc.3';
             $composer['require']['twig/twig'] = '^3.0';
         }
 
         if ($features['database'] ?? false) {
-            $composer['require']['lalaz/database'] = '^1.0.0-rc.2';
-            $composer['require']['lalaz/orm'] = '^1.0.0-rc.2';
+            $composer['require']['lalaz/database'] = '^1.0.0-rc.3';
+            $composer['require']['lalaz/orm'] = '^1.0.0-rc.3';
         }
 
         if ($features['auth'] ?? false) {
-            $composer['require']['lalaz/auth'] = '^1.0.0-rc.2';
+            $composer['require']['lalaz/auth'] = '^1.0.0-rc.3';
         }
 
         if ($features['cache'] ?? false) {
-            $composer['require']['lalaz/cache'] = '^1.0.0-rc.2';
+            $composer['require']['lalaz/cache'] = '^1.0.0-rc.3';
         }
 
         if ($features['queue'] ?? false) {
-            $composer['require']['lalaz/queue'] = '^1.0.0-rc.2';
+            $composer['require']['lalaz/queue'] = '^1.0.0-rc.3';
         }
 
         file_put_contents(
